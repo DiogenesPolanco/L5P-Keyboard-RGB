d7027d109685cb9704d14052889b9d31809a02e4
4JX <79868816+4JX@users.noreply.github.com>
2021-10-17 00:21:25 +0200

    Vendor stuffs

--- build-aux/cargo.sh
+++ build-aux/cargo.sh
@@ -5,18 +5,18 @@ export MESON_SOURCE_ROOT="$2"
 export CARGO_TARGET_DIR="$MESON_BUILD_ROOT"/target
 export CARGO_HOME="$CARGO_TARGET_DIR"/cargo-home
 export OUTPUT="$3"
-export BUILDTYPE="release"
+export BUILDTYPE="$4"
 export APP_BIN="$5"
 
 if [[ $BUILDTYPE = "release" ]]
 then
     echo "RELEASE MODE"
     cargo build --manifest-path \
-        "$MESON_SOURCE_ROOT"/Cargo.toml --release & \
+        "$MESON_SOURCE_ROOT"/Cargo.toml --release && \
         cp "$CARGO_TARGET_DIR"/release/"$APP_BIN" "$OUTPUT"
 else
     echo "DEBUG MODE"
     cargo build --manifest-path \
-        "$MESON_SOURCE_ROOT"/Cargo.toml --verbose & \
+        "$MESON_SOURCE_ROOT"/Cargo.toml --verbose && \
         cp "$CARGO_TARGET_DIR"/debug/"$APP_BIN" "$OUTPUT"
 fi
